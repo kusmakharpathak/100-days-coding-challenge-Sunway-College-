1350c0bec5b23bbc33e454abd87c6dd88143e79a
Kusmakhar Pathak <kusmakharpathak@outlook.com>
2020-07-28 10:37:16 +0545

    Day 2 Challenge related to string

---
+++ Day2.py
@@ -0,0 +1,26 @@
+# Challenge related to string
+
+string_1 = "Day 2 challenge related to String."
+string_2 = "Today I am plaining to manuplate the string and how it works"
+
+# print string_1
+print(string_1)
+
+# print string_1
+print(string_2)
+
+# adding string_1 and string_2
+merg_2_string = string_1+string_2
+print(merg_2_string)
+
+# picking only day feom string 1
+print(string_1[:3])
+
+# picking works from string_2 negative indexing
+print(string_1[:3-len(string_1)])
+
+# picking works from string_2
+print(string_2[len(string_2)-5:])
+
+# picking works from string_2 negative indexing
+print(string_2[-5:])
