74997e6865cc18b6792018f610af2d0509220668
Kusmakhar Pathak <kusmakharpathak@outlook.com>
2020-07-29 09:49:55 +0545

    Day 3 Challenge Related to Input value and it's type

---
+++ Day3.py
@@ -0,0 +1,27 @@
+# Input and it's Type
+
+# String Input
+name = input("Enter your name:\t")
+print(f"{name}\n")
+# type
+print(f"Data Type:\t{type(name)}\n")
+
+#integer input
+int_num = int(input("Enter intger number:\t"))
+print(f"{int_num}\n")
+# type
+print(f"Data Type:\t{type(int_num)}\n")
+
+#float input
+float_num = float(input("Enter decimal number:\t"))
+print(f"{float_num}\n")
+# type
+print(f"Data Type:\t{type(float_num)}\n")
+
+# List input
+list_in = [int(input(f"Enetr number {i+1}:\t")) for i in range(4)]
+print(f"{list_in}\n")
+# Type
+print(f"Data Type:\t{type(list_in)}\n")
+
+
