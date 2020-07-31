384a7557dd37673e5c8dbe9291733a2abb7ca275
Kusmakhar Pathak <kusmakharpathak@outlook.com>
2020-07-31 10:16:45 +0545

    Day 5 Challenge Related to Calculator using class and boject

--- Day5.py
+++ Day5.py
@@ -11,21 +11,26 @@ class Calculator:
         self.num1 = num1
         self.num2 = num2
 
+    # Addition method
     def Addition(self):
         return f"Sum of {self.num1} and {self.num2} = {self.num1 + self.num2}"
 
+    # Subtraction method
     def Subtraction(self):
         return f"Difference of {self.num1} and {self.num2} = {self.num1 - self.num2}"
 
+    # Multiply method
     def Multiply(self):
         return f"Product of {self.num1} and {self.num2} = {self.num1 * self.num2}"
 
+    # Division method
     def Division(self):
         try:
             return f"Division of {self.num1} and {self.num2} = {self.num1 / self.num2}"
         except Exception:
             raise Exception("Division by 0 is not allowed")
 
+    # Reminder method
     def Reminder(self):
         try:
             return f"Reminder of {self.num1} and {self.num2} = {self.num1 % self.num2}"
@@ -33,12 +38,19 @@ class Calculator:
             raise Exception("Division by 0 is not allowed")
 
 
+# Running Python program checking if __name__ == __main__
 if __name__ == "__main__":
+    # user input number 1
     num1 = int(input("Enter 1st number:\t"))
+    # user input operation +, -, *, /, %
     operation = input("Enter a operation you want to perform (+, _, *, /, %):\t")
+    # user input number 2
     num2 = int(input("Enter 2nd number:\t"))
+    # creating calculator object
     calculate = Calculator(num1, num2)
+    # checking operation
     if operation == '+':
+        # calling object methods calculate.Addition()
         print(calculate.Addition())
     elif operation == '-':
         print(calculate.Subtraction())
