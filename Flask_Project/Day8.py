5b67016fac790860fb1caf848b7f450d2dc90e00
Kusmakhar Pathak <kusmakharpathak@outlook.com>
2020-08-04 20:19:41 +0545

    Day 8 Template rendering and form data rendering

--- Flask_Project/Day8.py
+++ Flask_Project/Day8.py
@@ -8,17 +8,23 @@ Created:   3 August 2020
 """
     Welcome to restful API with Flask and MongoDB.
     This Exercises will go for 3 weeks to build a complete restful API and blogging site.
-    In this section I will demonstrate you to how to create WTForms, template rendering, Data base connection,
+    In this section I will demonstrate you to how to create WTForms, templates rendering, Data base connection,
     form validation and static file rendering like images, css, js. 
 """
 
-from flask import Flask
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
-
-@app.route('/')
+SECRET_KEY = 'development key'
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return "Welcome to restful API with Flask and MongoDB."
+    if request.method == 'POST':
+        f_name = request.form['f_name']
+        l_name = request.form['l_name']
+        address = request.form['address']
+        p_number = request.form['p_number']
+        return f"My name is {f_name} {l_name}.<br>I live in {address}.<br>My contact details is {p_number}.<br>Thank You..."
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
