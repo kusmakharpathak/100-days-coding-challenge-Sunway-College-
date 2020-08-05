3b385ce0b342ad200689b28d72d464f68d6180ee
Kusmakhar Pathak <kusmakharpathak@outlook.com>
2020-08-05 20:37:15 +0545

    Day 10 Fetch data from json and render to table in web browser

--- Flask_Project/app.py
+++ Flask_Project/app.py
@@ -12,19 +12,15 @@ Author :   Kusmakhar Pathak
     Templates and form rendering
 """
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+import json
 app = Flask(__name__)
 SECRET_KEY = 'development key'
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST':
-        f_name = request.form['f_name']
-        l_name = request.form['l_name']
-        address = request.form['address']
-        p_number = request.form['p_number']
-        return f"My name is {f_name} {l_name}.<br>I live in {address}.<br>My contact details is {p_number}.<br>Thank You..."
-    return render_template("index.html")
-
+    file = open('data.json')
+    data = json.load(file)
+    return render_template("index.html", data=(data['emp_details']))
 
 if __name__ == "__main__":
     app.run(debug=True)
